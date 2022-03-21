9d98d1dce4ad3deae36a81054945ffa864352e11
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2022-03-21 23:17:24 +0530

    Added iframes for spotify songs 🎵

--- src/App.js
+++ src/App.js
@@ -7,9 +7,35 @@ import './App.css';
 const TWITTER_HANDLE = 'sakshamm_9';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+//Test Gifs
+const TEST_GIFS = [
+	'https://open.spotify.com/embed/track/2Gd7x92yhLBkK29XenAjjX?si=5e8e22fc628548ac',
+	'https://open.spotify.com/embed/track/3uouaAVXpQR3X8RYkJyitQ',
+  'https://open.spotify.com/embed/track/6N22FZs2ZhPBYi3b9XPajV', 'https://open.spotify.com/embed/track/4iJyoBOLtHqaGxP12qzhQI'
+  
+
+]
+
 const App = () => {
   // State
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
+  
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  const sendGif = async () => {
+    if (inputValue.length > 10) {
+      console.log('Gif link:', inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue('');
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
 
   // Actions
   const checkIfWalletIsConnected = async () => {
@@ -43,6 +69,8 @@ const App = () => {
    * We will write the logic for this next!
    */
   const connectWallet = async () => {  const { solana } = window;
+  
+  
 
   if (solana) {
     const response = await solana.connect();
@@ -61,6 +89,36 @@ const App = () => {
     >
       Connect to Wallet
     </button>
+    
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      {/* Go ahead and add this input and button to start */}
+      <form
+       onSubmit={(event) => {
+       event.preventDefault();
+        sendGif();
+      }}
+      >
+      <input
+       type="text"
+       placeholder="Enter Song/Playlist linkyy!"
+       value={ inputValue }
+       onChange={ onInputChange }
+      />
+       <button type="submit" className="cta-button submit-gif-button">
+         Submit
+       </button>
+      </form>
+      <div className="gif-grid">
+        {gifList.map(gif => (
+          <div className="gif-item" key={gif}>
+          <iframe scrolling="yes" src={gif} width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 
     // UseEffects
@@ -71,6 +129,16 @@ const App = () => {
       window.addEventListener('load', onLoad);
       return () => window.removeEventListener('load', onLoad);
     }, []);
+    useEffect(() => {
+      if (walletAddress) {
+        console.log('Fetching GIF list...');
+        
+        // Call Solana program here.
+    
+        // Set state
+        setGifList(TEST_GIFS);
+      }
+    }, [walletAddress]);
   
   return (
     <div className="App">
@@ -84,6 +152,8 @@ const App = () => {
           </p>
           {/* Render your connect to wallet button right here */}
           {!walletAddress && renderNotConnectedContainer()}
+          {/* We just need to add the inverse here! */}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
